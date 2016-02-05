037517ca9acde02ffd51b091a3086ce247d720ac
Simon Jockers <hello@simonjockers.com>
2016-02-05 18:10:08 +0100

    Remove 'chapter' namespacing for shorter URLs.

--- correctiv_ttip_barometer/urls.py
+++ correctiv_ttip_barometer/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from .views import ChapterDetailView
 
 urlpatterns = [
-    url(r'^chapter/(?P<slug>[\w-]+)/$', ChapterDetailView.as_view(), name='ttip-chapter-detail'),
+    url(r'^(?P<slug>[\w-]+)/$', ChapterDetailView.as_view(), name='ttip-chapter-detail'),
 ]
