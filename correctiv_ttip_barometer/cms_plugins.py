058bd7047622e48447b350c7a60463c0b87dbadd
Simon Jockers <hello@simonjockers.com>
2016-02-05 18:30:21 +0100

    Add basic CMS plugin for displaying chapter lists.

---
+++ correctiv_ttip_barometer/cms_plugins.py
@@ -0,0 +1,22 @@
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+from django.utils.translation import ugettext as _
+
+from .models import Chapter
+
+
+@plugin_pool.register_plugin
+class TTIPBarometerListPlugin(CMSPluginBase):
+    """
+    Plugin for rendering a list of chapters
+    """
+
+    module = _("TTIP Barometer")
+    name = _("Chapter List")  # name of the plugin in the interface
+    render_template = "correctiv_ttip_barometer/plugin/list.html"
+
+    def render(self, context, instance, placeholder):
+        context = super(TTIPBarometerListPlugin, self)\
+            .render(context, instance, placeholder)
+        context['object_list'] = Chapter.objects.filter(published=True)
+        return context
