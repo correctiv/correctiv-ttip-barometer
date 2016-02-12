845971a480cdceaf8fa369e405e18e0b470d72d4
Simon Jockers <hello@simonjockers.com>
2016-02-12 17:45:30 +0100

    Admin: Make chapters sortable via drag & drop.

--- correctiv_ttip_barometer/admin.py
+++ correctiv_ttip_barometer/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
+from adminsortable2.admin import SortableAdminMixin
 
 from .models import Chapter
 
 
-class ChapterAdmin(admin.ModelAdmin):
+class ChapterAdmin(SortableAdminMixin, admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     list_display = ('title', 'updated', 'published')
 
