d3b9b9f6bc958d6a319b34cd87c0e3faa682004d
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2016-02-16 15:44:37 +0100

    Add a small chapter list plugin

--- correctiv_ttip_barometer/cms_plugins.py
+++ correctiv_ttip_barometer/cms_plugins.py
@@ -20,3 +20,20 @@ class TTIPBarometerListPlugin(CMSPluginBase):
             .render(context, instance, placeholder)
         context['object_list'] = Chapter.objects.all()
         return context
+
+
+@plugin_pool.register_plugin
+class TTIPBarometerSmallListPlugin(CMSPluginBase):
+    """
+    Plugin for rendering 5 chapters
+    """
+
+    module = _("TTIP Barometer")
+    name = _("Chapter List (5 items)")  # name of the plugin in the interface
+    render_template = "correctiv_ttip_barometer/plugin/list.html"
+
+    def render(self, context, instance, placeholder):
+        context = super(TTIPBarometerSmallListPlugin, self)\
+            .render(context, instance, placeholder)
+        context['object_list'] = Chapter.objects.all()[:5]
+        return context
